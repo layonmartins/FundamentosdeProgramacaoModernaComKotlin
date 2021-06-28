c7cd5c33586dd5449ca75b672553c08d5da4d26e
Layon Martins <layonmartins03@gmail.com>
2021-06-28 13:52:00 -0300

    Section 6: Lambdas

---
+++ src/main/kotlin/Tests.kt
@@ -0,0 +1,7 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+
+    println(1 != 2)
+
+}
---
+++ src/main/kotlin/lambdas/Lambda1.kt
@@ -0,0 +1,10 @@
+package lambdas
+
+fun main() {
+
+    val soma = { x: Int, y: Int ->
+        x + y
+    }
+
+    println(soma(4, 6))
+}
